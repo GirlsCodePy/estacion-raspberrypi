d2a1f1ba6e6fa2422030ac37939a0aaae21d4160
Gabriela Gaona <gaby.lorely@gmail.com>
2018-08-25 16:08:06 -0400

    correccion de temp-humedad

--- temp-humedad.py
+++ temp-humedad.py
@@ -3,4 +3,4 @@ import Adafruit_DHT
 
 while True:
     humedad, temperatura = Adafruit_DHT.read_retry(11,4)
-    print 'Temp: {0:0.1f} C  Humidity:{1:0.1f} %'.format(temperatura, humedad)
+    print ('Temp: {0:0.1f} C  Humidity:{1:0.1f} %'.format(temperatura, humedad))
